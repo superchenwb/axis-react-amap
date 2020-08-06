d27e4c7fba18e867a1f6a23414dd572df7321123
chen <chen1990wb@163.com>
2020-08-06 15:35:06 +0800

    fix:组件移植

---
+++ components/plugins/amap-plugin-autocomplete/index.js
@@ -0,0 +1,73 @@
+import React,{ Component }  from 'react';
+import windowVal from "../../../brick/utils"
+
+// https://lbs.amap.com/api/javascript-api/reference/search#m_AutocompleteOptions
+class Autocomplete extends Component {
+  constructor(props){
+    super(props);
+    this.onSelect = this.onSelect.bind(this);
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  componentDidMount(){
+    this.loadWithOptions(this.props);
+  }
+
+  loadWithOptions(props){
+    this.map = props.__map__;  
+    if (!this.map) {
+      console.log('组件必须作为 Map 的子组件使用');
+      return;
+    }
+    if(!props.options){
+      console.log('必须指定Autocomplete插件的配置参数');
+      return;
+    }
+    if(typeof props.onSelect !== 'function') {
+      console.log('必须指定onSelect回调函数');
+      return;
+    }
+    this.map.plugin(['AMap.Autocomplete', 'AMap.Geocoder'], () => {
+      let opts ={input: 'autoinput'};
+      Object.assign(opts, props.options);
+      console.log('init with options:', opts);
+      const auto = new windowVal.winVal.AMap.Autocomplete(opts);
+      this.map.addControl(auto);
+        windowVal.winVal.AMap.event.addListener(auto, "select", this.onSelect);//注册监听，当选中某条记录时会触发
+      const geoloc = new windowVal.winVal.AMap.Geocoder({});
+      console.log(geoloc);
+      this.map.addControl(geoloc);
+      this.geoloc = geoloc;
+      this.auto = auto;
+    });
+
+  }
+
+  onSelect(e) {
+    if(!e.poi.location) {
+      const poi = e.poi;
+      this.geoloc && this.geoloc.getLocation(poi.district + poi.address, (status, result) =>{
+        console.log('getLocation', status, result);
+        if(result && result.info === "OK"){
+          e.poi.location = result.geocodes[0].location;
+        }
+        this.props.onSelect(e);
+      })
+      return ;
+    }
+    this.props.onSelect(e);
+  }
+
+  render(){
+    if (this.props.options && this.props.options.input ){
+      // console.log('no input ctrl:', this.props, this.props.options);
+      return null;
+    }
+    return <input id='autoinput' placeholder={this.props.placeholder} style={this.props.style} />
+  }
+}
+
+module.exports = Autocomplete;
--- components/utils/APILoader.js
+++ components/utils/APILoader.js
@@ -1,101 +1,106 @@
 const DEFAULT_CONFIG = {
-  v: '1.4.0',
-  hostAndPath: 'webapi.amap.com/maps',
-  key: 'f97efc35164149d0c0f299e7a8adb3d2',
-  callback: '__amap_init_callback',
-  useAMapUI: false
+    v: '1.4.0',
+    hostAndPath: 'webapi.amap.com/maps',
+    key: 'f97efc35164149d0c0f299e7a8adb3d2',
+    callback: '__amap_init_callback',
+    useAMapUI: false
 }
 
 let mainPromise = null
 let amapuiPromise = null
 let amapuiInited = false
 export default class APILoader {
-  constructor({ key, useAMapUI, version, protocol }) {
-    this.config = { ...DEFAULT_CONFIG, useAMapUI, protocol }
-    if (typeof window !== 'undefined') {
-      if (key) {
-        this.config.key = key
-      } else if ('amapkey' in window) {
-        this.config.key = window.amapkey
-      }
-    }
-    if (version) {
-      this.config.v = version
-    }
-    this.protocol = protocol || window.location.protocol
-    if (this.protocol.indexOf(':') === -1) {
-      this.protocol += ':'
-    }
-  }
-
-  getScriptSrc(cfg) {
-    return `${this.protocol}//${cfg.hostAndPath}?v=${cfg.v}&key=${cfg.key}&callback=${cfg.callback}`
-  }
-
-  buildScriptTag(src) {
-    const script = document.createElement('script')
-    script.type = 'text/javascript'
-    script.async = true
-    script.defer = true
-    script.src = src
-    return script
-  }
-
-  getAmapuiPromise() {
-    if (window.AMapUI) {
-      return Promise.resolve()
-    }
-    const script = this.buildScriptTag(`${this.protocol}//webapi.amap.com/ui/1.0/main-async.js`)
-    const p = new Promise(resolve => {
-      script.onload = () => {
-        resolve()
-      }
-    })
-    document.body.appendChild(script)
-    return p
-  }
-
-  getMainPromise() {
-    if (window.AMap) {
-      return Promise.resolve()
-    }
-    const script = this.buildScriptTag(this.getScriptSrc(this.config))
-    const p = new Promise(resolve => {
-      window[this.config.callback] = () => {
-        resolve()
-        delete window[this.config.callback]
-      }
-    })
-    document.body.appendChild(script)
-    return p
-  }
-
-  load() {
-    if (typeof window === 'undefined') {
-      return null
-    }
-    const { useAMapUI } = this.config
-    mainPromise = mainPromise || this.getMainPromise()
-    if (useAMapUI) {
-      amapuiPromise = amapuiPromise || this.getAmapuiPromise()
-    }
-    return new Promise(resolve => {
-      mainPromise.then(() => {
-        if (useAMapUI && amapuiPromise) {
-          amapuiPromise.then(() => {
-            if (window.initAMapUI && !amapuiInited) {
-              window.initAMapUI()
-              if (typeof useAMapUI === 'function') {
-                useAMapUI()
-              }
-              amapuiInited = true
+    constructor({ key, useAMapUI, version, protocol, iFrameDom, iFrameWindow }) {
+        this.config = { ...DEFAULT_CONFIG, useAMapUI, protocol, iFrameDom, iFrameWindow}
+        if (typeof window !== 'undefined') {
+            if (key) {
+                this.config.key = key
+            } else if ('amapkey' in this.config.iFrameWindow) {
+                this.config.key = this.config.iFrameWindow.amapkey
             }
-            resolve()
-          })
-        } else {
-          resolve()
         }
-      })
-    })
-  }
+        if (version) {
+            this.config.v = version
+        }
+        this.protocol = protocol || this.config.iFrameWindow.location.protocol
+        if (this.protocol.indexOf(':') === -1) {
+            this.protocol += ':'
+        }
+    }
+
+    getScriptSrc(cfg) {
+        return `${this.protocol}//${cfg.hostAndPath}?v=${cfg.v}&key=${cfg.key}&callback=${cfg.callback}`
+    }
+
+    buildScriptTag(src) {
+        const iFrameDom= this.config.iFrameDom;
+        const iFrameWindow= this.config.iFrameWindow;
+        console.log(iFrameDom);
+        console.log(iFrameWindow);
+        console.log(iFrameWindow.document);
+        const script = iFrameWindow.document.createElement('script')
+        script.type = 'text/javascript'
+        script.async = true
+        script.defer = true
+        script.src = src
+        return script
+    }
+
+    getAmapuiPromise() {
+        if (this.config.iFrameWindow.AMapUI) {
+            return Promise.resolve()
+        }
+        const script = this.buildScriptTag(`${this.protocol}//webapi.amap.com/ui/1.0/main-async.js`)
+        const p = new Promise(resolve => {
+            script.onload = () => {
+                resolve()
+            }
+        })
+        this.config.iFrameWindow.document.body.appendChild(script)
+        return p
+    }
+
+    getMainPromise() {
+        if (this.config.iFrameWindow.AMap) {
+            return Promise.resolve()
+        }
+        const script = this.buildScriptTag(this.getScriptSrc(this.config))
+        const p = new Promise(resolve => {
+            this.config.iFrameWindow[this.config.callback] = () => {
+                resolve()
+                delete this.config.iFrameWindow[this.config.callback]
+            }
+        })
+        this.config.iFrameWindow.document.body.appendChild(script)
+        return p
+    }
+
+    load() {
+        if (typeof window === 'undefined') {
+            return null
+        }
+        const { useAMapUI } = this.config
+        mainPromise = mainPromise || this.getMainPromise()
+        if (useAMapUI) {
+            amapuiPromise = amapuiPromise || this.getAmapuiPromise()
+        }
+        return new Promise(resolve => {
+            mainPromise.then(() => {
+                if (useAMapUI && amapuiPromise) {
+                    amapuiPromise.then(() => {
+                        if (this.config.iFrameWindow.initAMapUI && !amapuiInited) {
+                            this.config.iFrameWindow.initAMapUI()
+                            if (typeof useAMapUI === 'function') {
+                                useAMapUI()
+                            }
+                            amapuiInited = true
+                        }
+                        resolve()
+                    })
+                } else {
+                    resolve()
+                }
+            })
+        })
+    }
 }
